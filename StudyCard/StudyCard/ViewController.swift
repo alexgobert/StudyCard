7601d31cb965b1e23b1dd1503dc0c08b514940bc
rhorvath02 <roberthorvath5@gmail.com>
2022-10-10 23:14:08 -0500

    Initial xcode

---
+++ StudyCard/StudyCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyCard
+//
+//  Created by A Horvath on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
