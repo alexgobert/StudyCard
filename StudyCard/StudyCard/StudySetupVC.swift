a355b2e6e3155f9243d1e30409e8706c79982e79
rhorvath02 <roberthorvath5@gmail.com>
2022-10-12 02:06:21 -0500

    Robert initial VCs

---
+++ StudyCard/StudyCard/StudySetupVC.swift
@@ -0,0 +1,29 @@
+//
+//  StudySetupVC.swift
+//  StudyCard
+//
+//  Created by A Horvath on 10/12/22.
+//
+
+import UIKit
+
+class StudySetupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
