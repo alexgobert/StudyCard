6307608e4605fc932288be3b1d27e6268d613e85
trannchris <traanchris@gmail.com>
2022-11-07 19:12:07 -0600

    Font Menu
    added font menu dropdown to settings vc

--- StudyCard/StudyCard/SettingsViewController.swift
+++ StudyCard/StudyCard/SettingsViewController.swift
@@ -6,24 +6,23 @@
 //
 
 import UIKit
+import DropDown
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var fontMenuDropDown: UIView!
+    
+    let fontMenu = DropDown()
+    let fontsArray = ["Font 1", "Font 2", "Font 3", "Font 4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        fontMenu.anchorView = fontMenuDropDown
+        fontMenu.dataSource = fontsArray
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showFontMenu(_ sender: Any) {
+        fontMenu.show()
     }
-    */
-
 }
