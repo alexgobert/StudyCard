d9b8cda365ab998b6ec877d30b6458a3609bea5d
trannchris <traanchris@gmail.com>
2022-11-10 02:53:57 -0600

    Worked on settings vc

--- StudyCard/StudyCard/SettingsViewController.swift
+++ StudyCard/StudyCard/SettingsViewController.swift
@@ -19,5 +19,28 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func showFontMenu(_ sender: Any) {
+        let fontController = UIAlertController(
+            title: "Font",
+            message: "Please select a font",
+            preferredStyle: .alert)
+        fontController.addAction(UIAlertAction(
+            title: "Comic Sans",
+            style: .default))
+        fontController.addAction(UIAlertAction(
+            title: "Helvetica",
+            style: .default))
+        fontController.addAction(UIAlertAction(
+            title: "Times New Roman",
+            style: .default))
+        fontController.addAction(UIAlertAction(
+            title: "Open Sans",
+            style: .default))
+        fontController.addAction(UIAlertAction(
+            title: "SF Pro",
+            style: .default))
+        fontController.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .cancel))
+        present(fontController, animated: true)
     }
 }
