242ab5916eac0ffa607dcc95898ce9731f1043e3
rhorvath02 <roberthorvath5@gmail.com>
2022-11-09 18:34:27 -0600

    Removed pods

--- StudyCard/StudyCard/SettingsViewController.swift
+++ StudyCard/StudyCard/SettingsViewController.swift
@@ -6,23 +6,18 @@
 //
 
 import UIKit
-import DropDown
 
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var fontMenuDropDown: UIView!
     
-    let fontMenu = DropDown()
     let fontsArray = ["Font 1", "Font 2", "Font 3", "Font 4"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fontMenu.anchorView = fontMenuDropDown
-        fontMenu.dataSource = fontsArray
     }
     
     @IBAction func showFontMenu(_ sender: Any) {
-        fontMenu.show()
     }
 }
